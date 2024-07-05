9b11cc9d5eb9c1d5a15d42d065ea6c60def1a0dd
Rafael Servelo <rafael.servelo@hotmail.com>
2024-07-05 01:19:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main class="bg-img">
+      <Home/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from "vue"
+  import Home from "@/components/Home.vue"
+  export default defineComponent({
+    components: {Home}
+  })
+</script>
