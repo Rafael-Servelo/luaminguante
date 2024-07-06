ba0aa4300079f99b59e917260e655d7f280a076e
Rafael Servelo <rafael.servelo@hotmail.com>
2024-07-06 14:07:50 -0300

    Menu lateral adicionado

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <v-app>
     <v-main class="bg-img">
-      <Home/>
+      <header-nav />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
   import {defineComponent} from "vue"
-  import Home from "@/components/Home.vue"
+  import HeaderNav from "@/components/Header.vue"
   export default defineComponent({
-    components: {Home}
+    components: { HeaderNav }
   })
 </script>
