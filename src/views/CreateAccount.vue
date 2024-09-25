555a20df25ee53108431d0ff465370ccdfc7b155
Rafael <rafael.servelo@dminer.com.br>
2024-09-25 12:31:15 -0300

    update

--- src/views/CreateAccount.vue
+++ src/views/CreateAccount.vue
@@ -9,70 +9,159 @@
     >
       <v-card
         color="var(--color-secondary)"
-        :width="$vuetify.display.mobile ? $vuetify.display.width - 20 : '500'"
+        :width="$vuetify.display.mobile ? $vuetify.display.width - 20 : '1000'"
         elevation="4"
         rounded="xl"
         class="ma-10"
       >
-        <v-card-title class="text-center">{{
+        <v-card-title class="text-center text-h3">{{
           "Criar Conta".toLocaleUpperCase()
         }}</v-card-title>
-        <v-card-text class="text-center" translate="yes"
+        <v-card-text class="text-center text-body-1" translate="yes"
           >Preencha o formulário abaixo para a criação de conta.</v-card-text
         >
         <v-form @submit.prevent="registerUser">
           <v-card-item>
-            <v-text-field
-              label="Nome Completo"
-              v-model="form.name"
-              type="text"
-              bg-color="white"
-              rounded
-              variant="solo"
-              :rules="rules"
-              theme="light"
-            />
-            <v-text-field
-              label="E-mail"
-              v-model="form.email"
-              type="email"
-              bg-color="white"
-              rounded
-              variant="solo"
-              :rules="rules"
-              theme="light"
-            />
-            <v-text-field
-              label="Senha"
-              v-model="form.password"
-              type="password"
-              bg-color="white"
-              rounded
-              variant="solo"
-              :rules="rules"
-              theme="light"
-            />
-            <v-text-field
-              label="Confirmar Senha"
-              v-model="form.confirmPassword"
-              type="password"
-              bg-color="white"
-              rounded
-              variant="solo"
-              :rules="rules"
-              theme="light"
-            />
-            <v-text-field
-              label="CPF"
-              v-model="form.cpf"
-              type="CPF"
-              bg-color="white"
-              rounded
-              variant="solo"
-              :rules="rules"
-              theme="light"
-            />
-            <input type="text" name="mask" v-model="form.cpf" v-mask="'000.000.000-00'" hidden>
+            <div class="flex w-100 justify-center">
+              <div class="w-100">
+                <div class="w-100 text-subtitle-2 mb-2">Dados Pessoais:</div>
+                <v-text-field
+                  label="Nome Completo"
+                  v-model="form.name"
+                  type="text"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                />
+                <v-text-field
+                  label="E-mail"
+                  v-model="form.email"
+                  type="email"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                />
+                <v-text-field
+                  label="Senha"
+                  v-model="form.password"
+                  type="password"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                />
+                <v-text-field
+                  label="Confirmar Senha"
+                  v-model="form.confirmPassword"
+                  type="password"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                />
+                <v-text-field
+                  label="CPF"
+                  v-model="form.cpf"
+                  type="CPF"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                />
+                <input
+                  type="text"
+                  name="mask"
+                  v-model="form.cpf"
+                  v-mask="'000.000.000-00'"
+                  hidden
+                />
+              </div>
+              <v-divider vertical class="mx-4" opacity=".3" color="white" />
+              <div class="w-100">
+                <div class="w-100 text-subtitle-2 mb-2">Endereço:</div>
+                <v-text-field
+                  label="CEP"
+                  v-model="form.address.cep"
+                  type="text"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  :rules="rules"
+                  theme="light"
+                  @blur="buscarCEP"
+                />
+                <input
+                  type="text"
+                  name="mask"
+                  v-model="form.address.cep"
+                  v-mask="'00000-000'"
+                  hidden
+                />
+                <div class="flex" style="gap: 1rem">
+                  <v-text-field
+                    width="280"
+                    label="Rua"
+                    v-model="form.address.rua"
+                    type="text"
+                    bg-color="white"
+                    rounded
+                    variant="solo"
+                    theme="light"
+                    :disabled="disableCEP"
+                  />
+                  <v-text-field
+                    label="Numero"
+                    v-model="form.address.numero"
+                    type="text"
+                    bg-color="white"
+                    rounded
+                    variant="solo"
+                    theme="light"
+                    :disabled="disableCEP"
+                  />
+                </div>
+                <v-text-field
+                  label="Bairro"
+                  v-model="form.address.bairro"
+                  type="text"
+                  bg-color="white"
+                  rounded
+                  variant="solo"
+                  theme="light"
+                  :disabled="disableCEP"
+                />
+                <div class="flex" style="gap: 1rem">
+                  <v-text-field
+                    width="280"
+                    label="Cidade"
+                    v-model="form.address.cidade"
+                    type="text"
+                    bg-color="white"
+                    rounded
+                    variant="solo"
+                    theme="light"
+                    :disabled="disableCEP"
+                  />
+                  <v-text-field
+                    label="Estado"
+                    v-model="form.address.uf"
+                    type="text"
+                    bg-color="white"
+                    rounded
+                    variant="solo"
+                    theme="light"
+                    :disabled="disableCEP"
+                  />
+                </div>
+              </div>
+            </div>
           </v-card-item>
           <v-card-item>
             <div class="d-flex w-100 align-center justify-center">
@@ -81,7 +170,6 @@
                 color="var(--color-green)"
                 rounded
                 type="submit"
-                @click="registerUser"
               />
             </div>
           </v-card-item>
@@ -113,6 +201,7 @@ export default defineComponent({
   data() {
     return {
       loadBtn: computed(() => store.state.auth.load),
+      disableCEP: true,
       form: {
         name: "",
         email: "",
@@ -141,6 +230,26 @@ export default defineComponent({
   methods: {
     registerUser() {
       console.log(this.form);
+    },
+    buscarCEP() {
+      let cep = this.form.address.cep.replace("-", "");
+
+      fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`)
+        .then((res) => res.json())
+        .then((data) => {
+          try {
+            this.form.address.rua = data.street;
+            this.form.address.bairro = data.neighborhood;
+            this.form.address.cidade = data.city;
+            this.form.address.uf = data.state;
+            this.disableCEP = false;
+          } catch (error) {
+            console.error(error);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
   },
 });
